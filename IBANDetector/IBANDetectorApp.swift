5b8ca9095711fc2c9c866d8e16aa611853bc5854
acs027 <alicihansarac@gmail.com>
2025-02-02 15:51:18 +0300

    Initial Commit

---
+++ IBANDetector/IBANDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBANDetectorApp.swift
+//  IBANDetector
+//
+//  Created by ali cihan on 2.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct IBANDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
